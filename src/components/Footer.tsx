5d7dc5f79e91527f10e815b216fdb587d0fcf90c
drowk <135056994+drowkid-1@users.noreply.github.com>
2023-07-12 21:48:35 -0600

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ import '../css/components.css'
 function Footer(): JSX.Element {
     return (
         <div className="Footer container">
-            <p>Made with <span className="heart">❤️</span> by <a href={"https://twitter.com/heysagnik"}>Sagnik Sahoo</a></p>
+            <p>Derechos Reservados <span className="heart">❤️</span> by <a href={"https://drowkid.bswc.net "}> DrowKid </a></p>
         </div>
     )
 }
